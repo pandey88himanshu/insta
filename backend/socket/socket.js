1a3d1d80cddc080f2d263129872e6d05b2168226
pandey88himanshu <pandey88himanshu@gmail.com>
2025-01-31 03:19:09 +0530

    cors

--- backend/socket/socket.js
+++ backend/socket/socket.js
@@ -1,13 +1,14 @@
 import { Server } from "socket.io";
 import express from "express";
 import http from "http";
-
+import dotenv from "dotenv";
 const app = express();
 const server = http.createServer(app);
 
+dotenv.config();
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:5173", // Fixed CORS URL without leading space
+    origin: process.env.URL, // Fixed CORS URL without leading space
     methods: ["GET", "POST"],
   },
 });
